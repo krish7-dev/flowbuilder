5772b54401bae4d9ef7a933f059949b14256ed06
krish7-dev <krishnaanilkumar.dev@gmail.com>
2024-05-16 02:16:49 +0530

    routing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+import Routing from './Routing';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <Routing/>
   </React.StrictMode>
 );
 
